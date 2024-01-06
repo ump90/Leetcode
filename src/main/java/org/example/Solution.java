d4221bd09e934f085617f934919637f4191a20bb
ump90 <ump90@protonmail.com>
2024-01-06 23:51:00 +0800

    453

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Solution {
+  private static Solution solution;
+
+  public static Solution getInstant() {
+    if (solution == null) {
+      solution = new Solution();
+    }
+    return solution;
+  }
+
+  public int minMoves(int[] nums) {
+    int min = nums[0];
+    int counts = 0;
+    for (int num : nums) {
+      min = Math.min(num, min);
+      counts = counts + num;
+    }
+    return counts - min * nums.length;
+  }
+
+}
---
+++ src/test/java/org/example/SolutionTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class SolutionTest extends TestCase {
+  public void testMinMoves() {
+    System.out.println(Solution.getInstant().minMoves(new int[] {1, 2, 3}));
+  }
+}
